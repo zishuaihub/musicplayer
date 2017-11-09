f8b3a3b4353f0ac9dd042373634ee1441956e5f3
WangZishuai <2354377285@qq.com>
2017-11-09 17:25:25 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    hello,world!
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style lang="stylus">
+  @import "common/css/variable.styl"
+  @import "common/css/index.styl"
+#app {
+  color: $color-theme
+}
+</style>
